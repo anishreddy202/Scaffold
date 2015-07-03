385e3027f08854c4aecf4e653236b3b56e5d7aa2
Anish Reddy <anish.m.reddy@verizon.com>
2015-07-03 02:28:33 -0500

    Error View Added

--- server/routes.ts
+++ server/routes.ts
@@ -9,22 +9,17 @@ module.exports = function(app){
 	app.route('/api').get((req, res) => {
 		res.json({"name": pkg.name, "version": "v1", "rev": pkg.version });
 	});
-  
-	// app.route('/').get((req,res) =>{
-	// 	//res.sendFile(app.get('appPath') + '/index.html',{ root: configJson.root });
-	// 	res.sendFile('index.html');
-	// });
-	
-	// app.get('/',function(req,res){
-	//   res.sendFile(app.get('appPath') + '/home.html',{ root: configJson.root });
-	// });
-	
+
 	app.get('/',function(req,res){
-	  res.sendFile('server/home.html',{ root: './' });
+	  res.sendFile('server/Home/home.html',{ root: './' });
 	});
 	
 	app.get('/myapps',function(req,res){
-	  res.sendFile('server/myapps.html',{ root: './' });
+	  res.sendFile('server/MyApps/myapps.html',{ root: './' });
+	});
+	
+	app.use(function(req, res) {
+      res.sendFile('server/Error/error.html',{ root: './' });
 	});
 	
 }
